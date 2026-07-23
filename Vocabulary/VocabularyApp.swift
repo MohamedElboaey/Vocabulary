e7d107bff6812d0291b5868f4d9ab5097f14c617
Mohamed Elboraey <Mohamed.HElboraey@eand.com.eg>
2026-07-23 19:34:51 +0300

    Initial Commit

---
+++ Vocabulary/VocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyApp.swift
+//  Vocabulary
+//
+//  Created by Mohamed Elboraey on 23/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocabularyTests/VocabularyTests.swift
@@ -0,0 +1,19 @@
+//
+//  VocabularyTests.swift
+//  VocabularyTests
+//
+//  Created by Mohamed Elboraey on 23/07/2026.
+//
+
+import Testing
+@testable import Vocabulary
+
+struct VocabularyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
